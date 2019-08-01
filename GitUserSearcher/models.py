4acb523d10ca00376e62111e8870492ada756146
fabrids23 <fabrids23@gmail.com>
2019-08-01 10:54:58 -0300

    Models created

--- GitUserSearcher/models.py
+++ GitUserSearcher/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    numberOfSearchs = models.IntegerField(default=0)
+    hireable = models.BooleanField(default=False)
+
+
+class SearchHistory(models.Model):
+    searcherUser = models.CharField(max_length=100)
+    gitUser = models.ForeignKey(User, on_delete=models.CASCADE)
+    time = models.DateField
