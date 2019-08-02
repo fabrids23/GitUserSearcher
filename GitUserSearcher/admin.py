4c926ca59d3735e557e80e66d04622090bbbe456
juan pisani <juanquipisani99@gmail.com>
2019-08-02 09:46:54 -0300

    Start on admin

--- GitUserSearcher/admin.py
+++ GitUserSearcher/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, SearchHistory
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    fields = ['username', 'numberOfSearchs', 'hireable']
+
+
+admin.site.register(User, UserAdmin)
